eddb0176deb41b8af7078ed074c9686d508bf90b
Joshua Ferreira <kointaker@Joshuas-MacBook-Air-10.local>
2025-11-28 18:36:22 -0500

    Added new modules to requirements.txt, added draft building function (Work in progress), added separate file for centurion functions for improved readability in the future

--- centurion/centurion.py
+++ centurion/centurion.py
@@ -8,6 +8,7 @@ from typing import List, Optional, Tuple
 from tqdm import tqdm
 from time import sleep
 
+
 # Gmail API modules
 # Import errors if any are because modules are in virtual environment and not the computer itself
 from google.auth.transport.requests import Request
@@ -15,7 +16,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-
+from email.message import EmailMessage
 
 
 # If modifying these scopes, delete the file token.json.
@@ -246,7 +247,6 @@ def query_search(service, user_id, keyword, amount) -> list[str]:
         print("")
     return [m["id"] for m in msgs]
 
-
 def inbox_choice(usrz):
     # Parses usrz and returns inbox selection
     if usrz == 1:
@@ -263,6 +263,45 @@ def inbox_choice(usrz):
         return "UNREAD"
     elif usrz == 7:
         return "STARRED"
+
+def gmail_create_draft(creds):
+  # Email Drafting
+
+  try:
+    # create gmail api client
+    service = build("gmail", "v1", credentials=creds)
+
+    message = EmailMessage()
+
+    message.set_content("This is automated draft mail")
+
+    message["To"] = "gduser1@workspacesamples.dev"
+    message["From"] = "gduser2@workspacesamples.dev"
+    message["Subject"] = "Automated draft"
+
+    # encoded message
+    encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+    create_message = {"message": {"raw": encoded_message}}
+    # pylint: disable=E1101
+    draft = (
+        service.users()
+        .drafts()
+        .create(userId="me", body=create_message)
+        .execute()
+    )
+
+    print(f'Draft id: {draft["id"]}\nDraft message: {draft["message"]}')
+
+  except HttpError as error:
+    print(f"An error occurred: {error}")
+    draft = None
+
+  return draft
+
+
+
+
 
 
 def main():
@@ -296,6 +335,13 @@ Z - List Messages
             creds = flow.run_local_server(port=0)
         with open("token.json", "w") as token:
             token.write(creds.to_json())
+
+
+
+
+    if usr == "V":
+        service = build_service(creds)
+        gmail_create_draft(creds)
 
     if usr == "W":
         service = build_service(creds)
